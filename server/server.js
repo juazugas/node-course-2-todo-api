679ba7964df4eaccd66762a651cb21b76fb60a5b
juazugas <juazuri@gmail.com>
2017-02-18 11:17:21 +0100

    ammend: Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,6 +55,31 @@ app.get('/todos/:todoId', (req, res) => {
 
 });
 
+app.delete('/todos/:todoId', (req, res) => {
+  var todoId = req.params.todoId;
+
+  //validate id , if is not valid return 404
+  if (!ObjectID.isValid(todoId)) {
+    return res.status(404).send({
+      message: `Id not valid ${todoId}`
+    });
+  }
+
+  //remove by id
+  Todo.findOneAndRemove(todoId).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({
+        message: `Id not found ${todoId}`
+      });
+    }
+    // success
+    res.send(todo);
+  }, (e) => {
+    // 400 with empty body
+    res.status(400).send(e);
+  });
+});
+
 app.listen(PORT , () => {
   console.log(`Started up at port ${PORT}`);
 });
