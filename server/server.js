ed35de451ba86cc89d5581a7183350f55e88ab8c
juazugas <juazuri@gmail.com>
2017-02-22 19:44:05 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -132,6 +132,21 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 })
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
+});
+
 app.listen(PORT , () => {
   console.log(`Started up at port ${PORT}`);
 });
